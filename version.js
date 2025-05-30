7a129433e64a94fadb0baac1909143af64502dff
uonick <hello@uonick.com>
2025-05-30 15:30:46 +0500

    Добавлены зависимости date-fns и fs-extra, изменен скрипт сборки для генерации файла версии в директории dist.

--- version.js
+++ version.js
@@ -1,16 +1,20 @@
 /* eslint-disable no-undef */
-const { writeFile } = require('fs-extra')
-const { format } = require('date-fns')
-const { version } = require('./package.json')
+import fs from 'fs-extra'
+import { format } from 'date-fns'
+
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'))
+const { version } = pkg
 const updated = format(Date.now(), 'dd.MM.yyyy')
 
 async function makeVersionFile() {
   const currentVersion = { version, updated }
 
-  await writeFile('./public/version.json', JSON.stringify(currentVersion), {
+  await fs.writeFile('./dist/version.json', JSON.stringify(currentVersion), {
     encoding: 'utf-8',
     flag: 'w',
   })
+
+  console.log('Version file created')
 }
 
 console.log('Creating version file...')
